96790b023c553b1f44f83fdd3afa61e92464997f
Mary Rose Cook <maryrosecook@maryrosecook.com>
2012-12-24 13:48:29 +0000

    delete redundant array_concat

--- array_concat.rb
+++
@@ -1,9 +0,0 @@
-a = ["a", ["b", "c", "d"], "e", ["f"], ["g", ["h", "i"], "j", "k"], "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-def concat(a)
-  return "" if a.first.nil?
-  return concat(a.first) + concat(a[1..-1]) if a.first.kind_of?(Array)
-  a.first + concat(a[1..-1])
-end
-
-puts concat(a)
